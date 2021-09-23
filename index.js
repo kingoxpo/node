e41ec535ae92f0b9bfed4b8777e9234f291b4483
aiden <kingoxpo@gamil.com>
2021-09-23 16:57:59 +0900

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express()
 const port = 3000
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://aiden:<password>@cluster0.lowzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://aiden:tnsqhr8516@cluster0.lowzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
