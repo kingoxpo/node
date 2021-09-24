7a88ca51a526f9634de6b7106e0e53c003da382d
aiden <kingoxpo@gamil.com>
2021-09-24 17:03:28 +0900

    secure info update

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://aiden:tnsqhr8516@cluster0.lowzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.send('hi!!!!')
+})
+
+app.post('/register', (req, res) => {
+  
+  //회원가입 시 필요한 정보들을 client에서 가져올 시 해당 정보를 DB에 저장한다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
